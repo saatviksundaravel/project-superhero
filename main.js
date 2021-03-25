581ff7ceece24addb8c03402d4b3408ed7dafcc0
saatviksundaravel <77335691+saatviksundaravel@users.noreply.github.com>
2021-03-25 22:10:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var canvas=new fabric.Canvas("myCanvas");
 var playerX=10;
 var playerY=10;
-var newImgWidth=40;
-var newImgheight=40;
+var blockImageWidth=40;
+var blockImageHeight=40;
 var playerImg=null;
 var newImg=null;
 
@@ -21,11 +21,11 @@ function player_update(){
 
 }
 
-function new_image(newImg){
+function uploadImage(newImg){
     fabric.Image.fromURL(newImg,function initBlock(img){
         var newObject=img;
-        newObject.scaleToWidth(newImgWidth);
-        newObject.scaleToHeight(newImgheight);
+        newObject.scaleToWidth(blockImageWidth);
+        newObject.scaleToHeight(blockImageHeight);
         newObject.set({
             top:playerY,
             left:playerX
@@ -33,3 +33,92 @@ function new_image(newImg){
         canvas.add(newObject);
     });
 }
+
+window.addEventListener("keydown",onKeydown);
+
+function onKeydown(e){
+
+    var keyPressed=e.keyCode;
+
+    if(e.shiftKey=true && keyPressed==80){
+
+        blockImageHeight=blockImageHeight+10;
+        blockImageWidth=blockImageWidth+10;
+        refreshBlockSize();
+        console.log("Shift+p detected")
+    }
+
+    if(e.shiftKey==true && keyPressed==77){
+        blockImageWidth=blockImageWidth-10;
+        blockImageHeight=blockImageHeight-10;
+        refreshBlockSize();
+    }
+
+    if(keyPressed==38){
+        up();
+        console.log("up");
+    }
+
+    if(keyPressed==40){
+        down()
+        console.log("down");
+    }
+
+    if(keyPressed==37){
+        left();
+        console.log("left");
+    }
+
+    if(keyPressed==39){
+        right();
+        console.log("right");
+    }
+
+    if(keyPressed==72){
+        uploadImage('captain_america_left_hand.png')
+        console.log("h");
+    }
+
+    if(keyPressed==76){
+        uploadImage('hulk_legs.png');
+        console.log("l");
+    }
+
+    if(keyPressed==70){
+        uploadImage('ironman_face.png');
+        console.log("f");
+    }
+
+
+    if(keyPressed==66){
+        uploadImage('spiderman_body.png');
+        console.log("b");
+    }
+
+    if(keyPressed==82){
+        uploadImage('thor_right_hand.png');
+        console.log("r");
+    }
+} 
+
+function up(){
+
+}
+
+function down(){
+
+}
+
+function right(){
+
+}
+
+function left(){
+
+}
+
+function refreshBlockSize(){
+    document.getElementById("current_height").innerHTML=blockImageHeight;
+    document.getElementById("current_width").innerHTML=blockImageWidth;
+}
+
